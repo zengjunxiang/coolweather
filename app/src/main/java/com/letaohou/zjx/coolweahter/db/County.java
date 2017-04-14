2956c5af7f502e2260274a0dec24e51fd8d0d438
zengjunxiang <674334638@qq.com>
2017-04-14 11:42:38 +0800

    添加省份，城市，县市实体类

---
+++ app/src/main/java/com/letaohou/zjx/coolweahter/db/County.java
@@ -0,0 +1,38 @@
+package com.letaohou.zjx.coolweahter.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by ZJX on 2017/4/14.
+ */
+
+public class County extends DataSupport {
+
+    private int id;
+    private String countyName;
+    private int cityId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCountyName() {
+        return countyName;
+    }
+
+    public void setCountyName(String countyName) {
+        this.countyName = countyName;
+    }
+
+    public int getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+}
